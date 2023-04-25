7635aa7abb03872a7b367c66a329bb427f277c93
Kyasha <128889938+kyashakya@users.noreply.github.com>
2023-04-25 12:18:17 -0700

    Add files via upload

---
+++ exercise1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    float cm,inches;
+    cout<<"Enter a length in cm:";
+    cin>>cm;
+    inches=cm/2.54;
+    cout<<"Length in inches is "<<endl<<inches;
+}
